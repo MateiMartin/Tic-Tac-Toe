729d25e09302a8447ff553511b79d570245b2847
Martin Matei <martin.matei@gmail.com>
2023-07-30 00:08:04 +0300

    finished the start page UI

--- Client/src/StartPage/StartPage.jsx
+++ Client/src/StartPage/StartPage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useState } from "react";
 import './StartPage.css';
+import { FiArrowLeft, FiArrowRight } from "react-icons/fi";
 
 const StartPage = ({ setRoute }) => {
     const [profileNumber, setProfileNumber] = useState(0);
@@ -8,7 +9,11 @@ const StartPage = ({ setRoute }) => {
     return (
         <div className="page">
             <div className="profile">
-                <img src={`https://robohash.org/${profileNumber}/?set=set5`} alt="imge" />
+                <div className="img-arrow">
+                    <FiArrowLeft size={40} color="#FFFFFF" style={{ cursor: 'pointer', userSelect: 'none' }} onClick={() => setProfileNumber(profileNumber - 1)} />
+                    <img src={`https://api.multiavatar.com/${profileNumber}.svg`} alt="imge" />
+                    <FiArrowRight size={40} color="#FFFFFF" style={{ cursor: 'pointer', userSelect: 'none' }} onClick={() => setProfileNumber(profileNumber + 1)} />
+                </div>
                 <input type="text" placeholder="Name..." />
             </div>
 
