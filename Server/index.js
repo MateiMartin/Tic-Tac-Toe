b793f88158dee71af2cfa345377e23766837f634
Martin Matei <martin.matei@gmail.com>
2023-07-31 00:11:19 +0300

    made the start page responsive

--- Server/index.js
+++ Server/index.js
@@ -8,7 +8,7 @@ const app = express();
 const httpServer = http.createServer(app);
 const io = new Server(httpServer, {
     cors: {
-        origin: ["http://localhost:5173"]
+        origin: ["http://localhost:5173", 'https://mateimartin.github.io/Tic-Tac-Toe/']
     }
 });
 
