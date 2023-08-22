4120e0d7537df23c7a979dc4258e5cc38808e6db
Martin Matei <martin.matei@gmail.com>
2023-08-22 14:11:48 +0300

    small bug fix

--- Server/server.js
+++ Server/server.js
@@ -105,7 +105,7 @@ io.on('connection', (socket) => {
     socket.on('room-leave', (room) => {
         // Emit 'user-disconnected' event to all players in the room
         // io.to(room.id).emit('user-disconnected');
-        socket.broadcast.emit('user-disconnected');
+        socket.to(room.id).emit('user-disconnected');
 
         // Loop through each socket in the room and make them leave
         const socketsInRoom = io.sockets.adapter.rooms.get(room.id);
